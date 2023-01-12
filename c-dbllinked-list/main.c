2aba75a4a18a5f2128fd4243a1decd2563040b30
Tomas Surovec <tomas.surovec87@gmail.com>
2023-01-12 23:00:56 +0100

    c-dbllinked - finds

--- c-dbllinked-list/main.c
+++ c-dbllinked-list/main.c
@@ -20,9 +20,8 @@ void print_list_forward(dlnode *start);
 //void print_list_backward(dlnode *start);
 int append(dlnode *list, dlnode *node);
 
-// todo find
-//dlnode* find(int index);
-//dlnode* find(const char *str);
+dlnode* find_by_index(dlnode *list, int index);
+dlnode* find_by_string(dlnode *list, const char *str);
 
 int delete(dlnode *node);
 
@@ -37,7 +36,6 @@ int main(int argc, const char **argv) {
     append(list, third);
     append(list, create("fourth"));
     //printf("1>%s\n", list->str);
-    
 
     print_list_forward(list);
     delete(third);
@@ -84,6 +82,29 @@ int append(dlnode *list, dlnode *node) {
     dlnode *end = list;
     while (end->next) end = end->next;
     return insert_after(end, node);
+}
+
+dlnode* find_by_index(dlnode *list, int index) {
+    if (list == NULL)
+        return NULL;
+        
+    if (index == 0)
+        return list;
+    
+    if (index > 0)
+        return find_by_index(list->next, index - 1);
+    else // advance in other direction
+        return find_by_index(list->prev, index + 1);
+}
+
+dlnode* find_by_string(dlnode *list, const char *str) {
+    if (list == NULL)
+        return NULL;
+        
+    if (strcmp(str, list->str) == 0)
+        return list;
+    
+    return find_by_string(list->next, str);
 }
 
 int delete(dlnode *node) {
